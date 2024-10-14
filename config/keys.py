367c5d3fdfab0e6dcd5706f6a2d8ca53b84c8479
Janmajay Kumar <106250110+QED137@users.noreply.github.com>
2024-10-14 22:46:09 +0200

    Delete config/keys.py

--- config/keys.py
+++
@@ -1,9 +0,0 @@
-Api_key = "172b18f956a2c3db37af6eb7854e27dd"
-Air_API="687292277emsh6620811a3972b04p1a4ee9jsn8c02f9bc139b"
-
-
-user = "root"
-password = "mysqldb"
-host = "34.76.110.104"
-database = "google_cloud_DB"
-port = 3306
